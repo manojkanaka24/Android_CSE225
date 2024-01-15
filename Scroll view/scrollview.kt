3178f65f1559af6f3b3c89fa06b947d52b54d073
Manoj Kanaka DH <134923961+manojkanaka24@users.noreply.github.com>
2024-01-15 12:58:28 +0530

    scrollview.kt

---
+++ Scroll view/scrollview.kt
@@ -0,0 +1,11 @@
+package com.example.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scrollviewxml : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrollview)
+    }
+}
